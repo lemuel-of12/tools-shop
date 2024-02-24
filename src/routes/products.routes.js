ad0f266c636c4c91e02b3c6fbb3c6a72fd67749b
lemuel_of <151422996+lemuel-of12@users.noreply.github.com>
2024-02-24 09:26:39 -0600

    Add files via upload

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,9 @@
-import { Router } from "express";
+import  Router  from "express";
 import {getAll, getOne, insertOne, updateOne, deleteOne} from '../controllers/products.controllers.js';
 const router = Router();
 router.get('/', getAll);
 router.get('/:barcode',getOne);
 router.post('/',insertOne);
 router.post('/:barcode',updateOne);
-router.delete('/:barcode',deleteOne);
+router.post('/:barcode/delete',deleteOne);
 export default router;
-router.post("/:barcode", updateOne)
-router.get("/delete/:barcode", deleteOne)
